041bdcf98a877d34d6fe700be75465ec94dcef37
david cochran <about.trout@gmail.com>
2014-10-29 19:35:16 -0500

    add high/low temperature

--- src/main/scala/DataTypes.scala
+++ src/main/scala/DataTypes.scala
@@ -2,25 +2,46 @@ package io.forecats
 
 import spray.http.HttpResponse
 import spray.httpx.unmarshalling.{FromResponseUnmarshaller, MalformedContent}
-import spray.json.DefaultJsonProtocol
-import spray.json.lenses.JsonLenses._
+import argonaut._, Argonaut._
 
-object DataTypes extends DefaultJsonProtocol {
+object DataTypes {
 
   case class Forecast(
     summary: String,
     icon: String,
-    temperature: Double,
-    apparentTemperature: Double
+    temperature: Int,
+    apparentTemperature: Int,
+    temperatureMin: Int,
+    temperatureMax: Int
   )
 
-  implicit def forecastFormat = jsonFormat4(Forecast)
+  implicit def forecastDecodeJson = DecodeJson[Forecast](c =>
+    for {
+      summary <- (c --\ "currently" --\ "summary").as[String]
+      icon <- (c --\ "currently" --\ "icon").as[String]
+      temperature <- (c --\ "currently" --\ "temperature").as[Int]
+      apparentTemperature <- (c --\ "currently" --\ "apparentTemperature").as[Int]
+      temperatureMin <- ((c --\ "daily" --\ "data" =\ 0) --\ "temperatureMin").as[Int]
+      temperatureMax <- ((c --\ "daily" --\ "data" =\ 0) --\ "temperatureMax").as[Int]
+    } yield Forecast(summary, icon, temperature, apparentTemperature, temperatureMin, temperatureMax)
+  )
+
+  implicit def forecastEncodeJson = EncodeJson[Forecast](
+    (f: Forecast) => ("summary" := f.summary)
+      ->: ("icon" := f.icon)
+      ->: ("temperature" := f.temperature)
+      ->: ("apparentTemperature" := f.apparentTemperature)
+      ->: ("temperatureMin" := f.temperatureMin)
+      ->: ("temperatureMax" := f.temperatureMax)
+      ->: jEmptyObject
+  )
 
   implicit val forecastUnmarshaller = new FromResponseUnmarshaller[Forecast] {
-    def apply(response: HttpResponse) = try {
-      Right(response.entity.asString.extract[Forecast]('currently))
-    } catch { case x: Throwable =>
-      Left(MalformedContent("Could not unmarshal Forecast", x))
+    def apply(response: HttpResponse) = {
+      Parse.decodeOption[Forecast](response.entity.asString) match {
+        case Some(forecast) => Right(forecast)
+        case None => Left(MalformedContent("Could not unmarshal Forecast"))
+      }
     }
   }
 }

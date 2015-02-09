6678bc3d34dd7e5e03352a028f38c6b6af5cf89b
david cochran <about.trout@gmail.com>
2015-02-08 20:58:40 -0600

    stashing more frontend work

--- src/main/resources/www/app.js
+++ src/main/resources/www/app.js
@@ -1,41 +1,96 @@
 (function() {
 
-  angular.module('forecats', [])
-    .factory('dataRequest', dataRequest)
-    .directive('skycon', skycon) 
-    .controller('catControl', catController)
-    .controller('searchControl', searchController)
-    .controller('weatherControl', weatherController)
-    .controller('creditsControl', creditsController);
+  angular
+    .module('forecats', ['ngRoute'])
+      .config(function($routeProvider) {
+        console.log('config');
+        console.log($routeProvider.when);
+      })
 
-  function dataRequest($http) {
-    var logError = function(rs, st, hs) {
-          // TODO; log error on backend, etc
-          // $http.post('/debug', {...});
-          return null;
-        },
-        handleResponse = function(res) {
-          return (res.status === 200
-            ? res.data
-            : logError(response, status, headers)
-          );
-        };
+      .factory('weatherUtil', weatherUtil)
+      .factory('catUtil', catUtil)
+      .factory('geoUtil', geoUtil)
 
+      .controller('catControl', catController)
+      .controller('searchControl', searchController)
+      .controller('weatherControl', weatherController)
+      .controller('creditsControl', creditsController)
+      
+      .directive('skycon', skycon) 
+      .filter('temp', tempFilter)
+
+      .run(init);
+
+  function coordinateControl($scope, $routeParams, $rootScope) {
+    console.log($routeParams);
+  }
+  
+  function catUtil($http, $rootScope) {
     return {
-      fromUrl: function(url) {
-        var prefix = 'http://logitank.net';
-        return $http.get(prefix + url).then(handleResponse);
+      random: function() {
+        $http.get('http://logitank.net/cats/random')
+          .then(function(res) {
+            $rootScope.$emit('CAT_ID', res.data);
+          });
       }
     };
   }
 
-  var skycons = new Skycons({ "color": "#231f20" });
+  function weatherUtil($http) {
+    var weatherFromCoordinates = function(lat, lng) {
+          return $http.get('http://logitank.net/weather/' + [lat,lng].join(','));
+        };
+
+    return {
+      fromCoordinates: weatherFromCoordinates
+    };
+
+  }
+
+  function geoUtil($rootScope) {
+    var fromCoordinates = function(lat, lng) {
+
+        },
+        fromQuery = function(query) {
+          var address = { address: query },
+              handler = function(xs) {
+                if(!xs.length) return;
+
+                var lat = xs[0].geometry.location.lat(),
+                    lng = xs[0].geometry.location.lng(),
+                    loc = xs[0].formatted_address;
+
+                $rootScope.$emit('COORDINATES', lat, lng);
+                $rootScope.$emit('LOCATION', loc);
+              };
+          
+          (new google.maps.Geocoder()).geocode(address, handler);
+        };
+      
+    return {
+      fromCoordinates: fromCoordinates,
+      fromQuery: fromQuery
+    };
+  }
+
+  function tempFilter() {
+    return function(t, degF) {
+      return (degF 
+        ? t
+        : Math.floor(5*(t-32)/9)
+      );
+    }
+  }
+
+  var skycons = new Skycons({ "color": "#312B27" });
+  skycons.play();
+
   function skycon() {
     return {
       restrict: 'EA',
       scope: { skycon: '=' },
       link: function(scope, elem) {
-        skycons.add(elem[0], Skycons.RAIN);
+        skycons.add(elem[0], scope.skycon);
 
         scope.$watch('skycon', function(skycon) { 
           skycons.set(elem[0], skycon);
@@ -44,50 +99,39 @@
     };
   }
 
-  function weatherController($scope, $rootScope, $location, dataRequest) {
-    var weatherFromCoordinates = function(lat, lng) {
-          dataRequest.fromUrl('/weather/' + [lat, lng].join(','))
-            .then(function(forecast) {
-              $scope.forecast = forecast;
-              $scope.currently = forecast.currently;
-            });
-        };
+  function weatherController($scope, $rootScope, $location, weatherUtil) {
+    
+    $scope.showHourly = true;
+    $scope.time12h = true;
+    $scope.degF = true;
 
     $rootScope.$on('COORDINATES', function(evt, lat, lng) {
-      console.log('got coordinates:', lat, lng);
+      console.log(lat, lng);
+      delete $scope.forecast;
 
-      $scope.forecast = null;
-      weatherFromCoordinates(lat, lng);
+      weatherUtil
+        .fromCoordinates(lat, lng)
+        .then(function(w) {
+          $scope.forecast = w;
+        });
     });
-
-    weatherFromCoordinates(30.2672, -97.7431);
-    /*
-    var hash = $location.hash();
-    console.log('hash:', hash);
-    if(hash) {
-      var ls = hash.split(',');
-      console.log('extracted from hash:', ls);
-      weatherFromCoordinates(ls[0], ls[1]);
-    }
-    */
-
-    $scope.showHourly = true;
-    $scope.toggleHourly = function() {
-      $scope.showHourly = !$scope.showHourly;
-    };
   }
 
-  function catController($scope, $rootScope, dataRequest) {
-    $scope.id = null;
+  function catController($scope, $rootScope, catUtil) {
     $rootScope.$on('CAT_ID', function(evt, id) {
-      $scope.id = id;
+      $scope.catId = id;
     });
 
-    dataRequest.fromUrl('/cats/random')
-      .then(function(id) {
-        //$scope.id = id;
-        $rootScope.$emit('CAT_ID', id);
-      });
+    $scope.canPlayType = (function() {
+      var video = document.createElement('video');
+      return (!video.canPlayType
+        ? {}
+        : /* codec support: */ {
+          mp4: video.canPlayType('video/mp4'),
+          webm: video.canPlayType('video/webm')
+        }
+      );
+    });
   }
 
   function creditsController($scope, $rootScope) {
@@ -103,18 +147,22 @@
     $scope.forecastHref = 'http://forecast.io';
   }
 
-  function searchController($scope, $rootScope, $location) {
-
-    // text displayed in the input box ...
+  function searchController($scope, $rootScope, geoUtil) {
     $scope.query = '';
-    // ... which we update when we get something 'official'
+    $scope.editLoc = false;
+
     $rootScope.$on('LOCATION', function(evt, loc) {
       $scope.query = loc;
     });
 
     $scope.handleSearch = function() {
-      console.log('search for:', $scope.query);
+      //console.log('search for:', $scope.query);
+      geoUtil.fromQuery($scope.query);
     };
+  }
+
+  function init(catUtil) {
+    catUtil.random();
   }
 
 }());

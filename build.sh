250ecb1a0d509fc40f00ed5852394b83410f231f
david cochran <about.trout@gmail.com>
2015-03-29 19:37:50 -0500

    more legit deployment logic

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ function deploy() {
   cd target/scala-2.11
   scp forecats.jar forecats:~/forecats/
   scp {resource_managed/main/resources/www/app.js,classes/www/{index.html,sprites.svg,style.css}} forecats:~/forecats/www
-  ssh forecats ~/forecats/run.sh
+  ssh forecats '~/forecats/run.sh'
   cd ../../
 }
 
